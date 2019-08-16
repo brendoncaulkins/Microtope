c9164ef732bae2fbd290ba308cd5a8d439ae206c
Twonki <Leonhard.Applis@Protonmail.com>
2019-08-16 21:47:45 +0200

    Re-moved privileges in own file, adjusted syntax to compile.

--- sql-files/Step02_createUser.sql
+++ sql-files/Step02_createUser.sql
@@ -1,12 +1,9 @@
 USE 'microtope';
 
-CREATE USER IF NOT EXISTS 'worker'@'%' IDENTIFIED VIA mysql_native_password BY "INeed2Write";
-GRANT select,insert, delete, update ON 'microtope'.* to 'worker'@'%';
+CREATE USER IF NOT EXISTS 'worker'@'%' IDENTIFIED BY 'INeed2Write';
 
 CREATE USER IF NOT EXISTS 'api'@'%' IDENTIFIED BY 'Need4Read';
-GRANT select ON 'microtope'.* TO 'api'@'%';
 
 CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admicromin';
-GRANT ALL PRIVILEGES ON 'microtope'.* TO 'admin'@'%';
 
 FLUSH PRIVILEGES;
--- sql-files/Step03_grantPrivileges.sql
+++ sql-files/Step03_grantPrivileges.sql
@@ -1,1 +1,10 @@
 Use microtope;
+
+GRANT USAGE ON microtope.* to 'worker'@'%';
+
+GRANT ALL PRIVILEGES ON microtope.* TO 'admin'@'%';
+
+GRANT SELECT ON microtope.* TO 'api'@'%';
+
+
+FLUSH PRIVILEGES;
