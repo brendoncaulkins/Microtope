80f035288042a5a775c2f249639d0882c959ffcd
Twonki <Leonhard.Applis@Protonmail.com>
2019-08-26 21:07:28 +0200

    FakeTeams and FakePlayers added

---
+++ src/app/models/faketeams.ts
@@ -0,0 +1,9 @@
+import {Team} from './Team.model';
+
+export const fakeTeams:Team[]=[
+    {team_id:1, team_name: "Rhinos"},
+    {team_id:2, team_name:"Microtopers"},
+    {team_id:3, team_name:""},
+    {team_id:-1, team_name:"Negative Nancies"},
+    {team_id:500, team_name:null}
+]
