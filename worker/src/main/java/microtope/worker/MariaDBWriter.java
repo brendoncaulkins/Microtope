3bca207594ebb386e140cba42449d32bf646446f
Twonki <Leonhard.Applis@Protonmail.com>
2019-09-01 10:38:35 +0200

    two minor fixes to compile again

--- worker/src/main/java/microtope/worker/MariaDBWriter.java
+++ worker/src/main/java/microtope/worker/MariaDBWriter.java
@@ -7,8 +7,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.time
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
