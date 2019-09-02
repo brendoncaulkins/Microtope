ec058d4017427d973660c7e2c135a2fad33b7a38
Twonki <Leonhard.Applis@Protonmail.com>
2019-09-02 19:16:38 +0200

    Added dummy db provider and injected it on getPlayers of playerservice

--- ui/src/app/services/databaseprovider.service.ts
+++ ui/src/app/services/databaseprovider.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable,Inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseproviderService {
 
-  constructor() { }
+  constructor(
+    @Inject('BACKEND_API_URL') private apiUrl: string,
+    @Inject('BACKEND_API_USER') private apiUser: string,
+    @Inject('BACKEND_API_PWD') private apiPwd: string
+  ) { }
+
+  public getUrl():string{ return this.apiUrl;}
 }
--- ui/src/app/services/player.service.ts
+++ ui/src/app/services/player.service.ts
@@ -3,15 +3,17 @@ import { Injectable } from '@angular/core';
 import {Player, IPlayer} from '../models/Player.model';
 import {fakePlayers} from '../models/fakePlayers'; 
 import { Observable,of } from 'rxjs';
+import { DatabaseproviderService } from './databaseprovider.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
-  constructor() { }
+  constructor(private databaseproviderservice:DatabaseproviderService) { }
 
   public getPlayers():Observable<Player[]>{
+    console.log("URL Found: " + this.databaseproviderservice.getUrl())
     return of(fakePlayers);
   }
 
