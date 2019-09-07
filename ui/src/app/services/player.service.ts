20ecb51e8b03301f7b20420358b5b1268c5b4bab
Twonki <Leonhard.Applis@Protonmail.com>
2019-09-07 11:32:19 +0200

    Added (unused) Healthcheck Service, extracted function from playerservice

--- ui/src/app/services/player.service.ts
+++ ui/src/app/services/player.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
-import {map, tap, switchMap,filter,first} from 'rxjs/operators';
+import {map, tap, switchMap} from 'rxjs/operators';
 
 import {Player} from '../models/Player.model';
-import {Observable, of} from 'rxjs';
+import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import { AppConfigService } from './app-config.service';
 
@@ -14,12 +14,6 @@ export class PlayerService {
   constructor(private config:AppConfigService, private http: HttpClient) { }
 
   public getPlayers(): Observable<Player[]> {
-    var health = this.config.loadAppConfig().pipe(
-      tap(x=> console.log(" I am healthchecking")),
-      switchMap( conf => this.http.get(conf.api_url+"/api/healthcheck")),
-      tap(x => console.log("Found:" + JSON.stringify(x)))
-    )
-
     return this.config.loadAppConfig().pipe(
       tap(con => console.log("Got Config with base_url:" + con.api_url)),
       map(con => con.api_url+"/api/player_summary"),
