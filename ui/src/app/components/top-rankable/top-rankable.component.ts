6231bb8b6fc3f308f10c22594c083a3e728ec3ab
flash-me <5240450+flash-me@users.noreply.github.com>
2019-09-09 23:22:09 +0200

    Make dis codebase awsum

--- ui/src/app/components/top-rankable/top-rankable.component.ts
+++ ui/src/app/components/top-rankable/top-rankable.component.ts
@@ -1,20 +1,21 @@
-import { Component,Input } from '@angular/core';
-import { Rankable } from 'src/app/models/IRankable';
-import { SelectedService } from 'src/app/services/selected.service';
-import { selectionComponent } from '../selectionComponent';
+import {Component, OnInit, OnDestroy, Input} from '@angular/core';
+import {Rankable} from 'src/app/models/IRankable';
+import {SelectedService} from 'src/app/services/selected.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-top-rankable',
   templateUrl: './top-rankable.component.html',
   styleUrls: ['./top-rankable.component.css']
 })
-export class TopRankableComponent<T extends Rankable,IPreviewable> extends selectionComponent<T> {
-  
-  @Input() items: T[];
-  constructor(private injectedSelectionService:SelectedService<T>) {super(injectedSelectionService);}
+export class TopRankableComponent<T extends Rankable, IPreviewable> {
+  private TOP_COUNT = 3;
+  sortedItems: Array<T> = [];
 
-  topN(items:T[],n:number):T[]{
-    return items.slice(0,n);
-    //return items.sort((i1,i2)=> i1.compare(i2)).slice(0,n);
+  @Input()
+  public set items(items: Array<T>) {
+    this.sortedItems = items.slice(0, this.TOP_COUNT);
   }
+
+  constructor(public selection: SelectedService<T>) { }
 }
