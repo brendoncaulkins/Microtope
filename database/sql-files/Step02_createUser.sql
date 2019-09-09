4dc2542e5b87099add735754ee8a4b7f376a3d50
Twonki <Leonhard.Applis@Protonmail.com>
2019-09-09 21:37:21 +0200

    Added Healthcheck user for database and healthcheck for dockerfile

--- database/sql-files/Step02_createUser.sql
+++ database/sql-files/Step02_createUser.sql
@@ -6,6 +6,8 @@ CREATE USER IF NOT EXISTS 'api'@'%' IDENTIFIED BY 'Need4Read';
 
 CREATE USER IF NOT EXISTS 'auditor'@'%' IDENTIFIED BY 'ARGU5';
 
+CREATE USER IF NOT EXISTS 'docker_health'@'localhost' IDENTIFIED BY 'showmelove';
+
 CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admicromin';
 
 FLUSH PRIVILEGES;
