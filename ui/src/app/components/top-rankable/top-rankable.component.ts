c9f348b5cf25923bed7f98b4eafe3e2e4b489a00
Twonki <Leonhard.Applis@Protonmail.com>
2019-09-09 23:22:21 +0200

    Added generic SelectionComponent

--- ui/src/app/components/top-rankable/top-rankable.component.ts
+++ ui/src/app/components/top-rankable/top-rankable.component.ts
@@ -1,33 +1,17 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component,Input } from '@angular/core';
 import { Rankable } from 'src/app/models/IRankable';
 import { SelectedService } from 'src/app/services/selected.service';
-import { Subscription } from 'rxjs';
+import { selectionComponent } from '../selectionComponent';
 
 @Component({
   selector: 'app-top-rankable',
   templateUrl: './top-rankable.component.html',
   styleUrls: ['./top-rankable.component.css']
 })
-export class TopRankableComponent<T extends Rankable,IPreviewable> implements OnInit,OnDestroy {
+export class TopRankableComponent<T extends Rankable,IPreviewable> extends selectionComponent<T> {
   
   @Input() items: T[];
-  constructor(private selection:SelectedService<T>) { }
-
-  selectionSub: Subscription;
-  selectedItem:T;
-
-  ngOnInit() {
-    this.selectionSub= this.selection.subject$.subscribe(
-      newSelection => {this.selectedItem = newSelection});
-  }
-
-  ngOnDestroy(){
-    this.selectionSub && this.selectionSub.unsubscribe();
-  }
-
-  onSelect(item:T): void {
-    this.selection.set(item);
-  }
+  constructor(private injectedSelectionService:SelectedService<T>) {super(injectedSelectionService);}
 
   topN(items:T[],n:number):T[]{
     return items.slice(0,n);
