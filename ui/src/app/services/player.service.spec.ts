ce9160cce730186d53410e2c83861f8afad8a722
Twonki <Leonhard.Applis@Protonmail.com>
2019-09-11 18:16:18 +0200

    apihealtcheck is now passing - nothing else, lol

--- ui/src/app/services/player.service.spec.ts
+++ ui/src/app/services/player.service.spec.ts
@@ -5,20 +5,31 @@ import { HttpClient } from '@angular/common/http';
 import { AppConfigService } from './app-config.service';
 
 describe('PlayerService', () => {
-  beforeAll(() => {
 
+  let playerService: PlayerService;
+  let valueServiceSpy: jasmine.SpyObj<AppConfigService>;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+
+  beforeAll(async () => {
+    const confSpy = jasmine.createSpyObj('AppConfigService', ['getValue']);
+    const httpSpy = jasmine.createSpyObj('HttpClient', ['get','put','push']);
+  
     TestBed.configureTestingModule({
       providers: [
-        {provide: HttpClient},
-        {provide: AppConfigService}
+        PlayerService,
+        { provide: AppConfigService, useValue: confSpy },
+        { provide: HttpClient, useValue: httpSpy}
       ]
     });
+
+    // Inject both the service-to-test and its (spy) dependency
+      valueServiceSpy = TestBed.get(AppConfigService);
+      httpClientSpy = TestBed.get(HttpClient);
+      playerService = TestBed.get(PlayerService);
   });
 
   it('should be created', () => {
     const service: PlayerService = TestBed.get(PlayerService);
     expect(service).toBeTruthy();
   });
-
 });
-
