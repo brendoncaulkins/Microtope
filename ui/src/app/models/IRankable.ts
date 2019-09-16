75369950f0e882e943a40664847e347354af31c1
Twonki <Leonhard.Applis@Protonmail.com>
2019-09-16 19:44:11 +0200

    Extracted compare and topN to utils folder, added tests for compare

--- ui/src/app/models/IRankable.ts
+++ ui/src/app/models/IRankable.ts
@@ -7,24 +7,4 @@ export abstract class Rankable implements IRankable {
   public abstract coins?: number;
   public abstract steps?: number;
 
-  public compare(other: Rankable): number {
-    if (other.coins && this.coins) {
-      if (other.coins > this.coins) {
-        return -1;
-      } else if (other.coins < this.coins) {
-        return 1;
-      } else if (other.steps && this.steps) {
-        if (other.steps > this.steps) {
-          return -1;
-        } else if (other.steps < this.steps) {
-          return 1;
-        } else {
-          return 0;
-        }
-      } else {
-        return 0;
-      }
-    }
-  }
-
 }
