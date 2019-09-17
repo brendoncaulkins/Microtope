edb667c279de1b878fb9f8167695d65fb94f78b7
Twonki <Leonhard.Applis@Protonmail.com>
2019-09-17 17:44:01 +0200

    refixed typos from new angular, back to generics!

--- ui/src/app/components/top-rankable/top-rankable.component.ts
+++ ui/src/app/components/top-rankable/top-rankable.component.ts
@@ -3,30 +3,32 @@ import { IRankable} from 'src/app/models/IRankable';
 import {SelectedService} from 'src/app/services/selected.service';
 import { Observable, Subscription } from 'rxjs';
 import { topN } from 'src/app/utils/IRankable.functions';
+import { IPreviewable } from 'src/app/models/IPreviewable';
 
 @Component({
   selector: 'app-top-rankable',
   templateUrl: './top-rankable.component.html',
   styleUrls: ['./top-rankable.component.css']
 })
-export class TopRankableComponent implements OnInit,OnDestroy{
+export class TopRankableComponent<T extends IPreviewable & IRankable> implements OnInit,OnDestroy{
   private TOP_COUNT = 3;
 
   selectionSub: Subscription;
-  selectedItem:IRankable;
+  selectedItem:T;
   
   rankingSub: Subscription;
-  sortedItems: IRankable[] = [];
+  sortedItems: T[] = [];
 
-  @Input() items: Observable<IRankable[]>;
+  @Input() items: Observable<T[]>;
 
-  constructor(public selection: SelectedService<IRankable>) { }
+  constructor(public selection: SelectedService<T>) { }
 
   ngOnInit() {
     this.selectionSub= this.selection.selected$.subscribe(
       newSelection => {this.selectedItem = newSelection});
+
     this.rankingSub = this.items.subscribe(
-      newItems => {this.sortedItems = topN(newItems,this.TOP_COUNT)} 
+      newItems => {this.sortedItems =  (topN(newItems,this.TOP_COUNT) as T[])  } 
     )
   }
 
@@ -35,7 +37,7 @@ export class TopRankableComponent implements OnInit,OnDestroy{
     this.rankingSub && this.rankingSub.unsubscribe();
   }
 
-  onSelect(item:IRankable): void {
+  onSelect(item:T): void {
     this.selection.select(item);
   }
 
