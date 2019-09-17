5c85b92cf030b58de0305fab3d1d82b2aed9e514
flash-me <5240450+flash-me@users.noreply.github.com>
2019-09-17 22:31:58 +0200

    Do more awsum stuff

--- ui/src/app/utils/IRankable.functions.ts
+++ ui/src/app/utils/IRankable.functions.ts
@@ -1,44 +1,58 @@
-import {IRankable} from "../models/IRankable"
+import {IRankable} from '../models/IRankable';
 
-export function compare(first: IRankable, second: IRankable): number
-{
-    const coinCompare = compareCoins(first,second);
-    return coinCompare==0 ?  compareSteps(first,second) : coinCompare
+export function compare(first: IRankable, second: IRankable): number {
+  const coinCompare = compareCoins(first, second);
+  return coinCompare === 0 ? compareSteps(first, second) : coinCompare;
 }
 
 function compareCoins(first: IRankable, second: IRankable): number {
-    if(!first.coins && !second.coins)
-        return 0;
-    if(first.coins && ! second.coins)
-        return -1;
-    if(second.coins && ! first.coins)
-        return 1;
-    
-    if(first.coins>second.coins)
-        return -1;
-    if(second.coins>first.coins)
-        return 1;
+  if (!first.coins && !second.coins) {
     return 0;
-}
+  }
+  if (first.coins && !second.coins) {
+    return -1;
+  }
+  if (second.coins && !first.coins) {
+    return 1;
+  }
 
+  if (first.coins > second.coins) {
+    return -1;
+  }
+  if (second.coins > first.coins) {
+    return 1;
+  }
+  return 0;
+}
 
 function compareSteps(first: IRankable, second: IRankable): number {
-    if(!first.steps && !second.steps)
-        return 0;
-    if(first.steps && ! second.steps)
-        return -1;
-    if(second.steps && ! first.steps)
-        return 1;
-    if(first.steps>second.steps)
-        return -1;
-    if(second.steps>first.steps)
-        return 1;
+  if (!first.steps && !second.steps) {
     return 0;
+  }
+  if (first.steps && !second.steps) {
+    return -1;
+  }
+  if (second.steps && !first.steps) {
+    return 1;
+  }
+  if (first.steps > second.steps) {
+    return -1;
+  }
+  if (second.steps > first.steps) {
+    return 1;
+  }
+  return 0;
 }
 
-export function topN(rankables : IRankable[], n:number):IRankable[]{
-    return rankables
-        .slice() // Creates a working copy 
-        .sort((a,b)=>compare(a,b)) //Sorts with compare function from above
-        .slice(0,n);//Takes top n
+export const topNCurry = (n: number) =>
+  (rankables: IRankable[]) =>
+    rankables.slice()
+      .sort((a, b) => compare(a, b))
+      .slice(0, n);
+
+export function topN(rankables: IRankable[], n: number): IRankable[] {
+  return rankables
+    .slice()                                  // Creates a working copy
+    .sort((a, b) => compare(a, b)) // Sorts with compare function from above
+    .slice(0, n);                            // Takes top n
 }
